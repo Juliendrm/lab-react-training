4161f16fe70985327259f119eb8be11f0b5483a9
Julien <benedetjulien@gmail.com>
2022-09-19 09:34:30 +0200

    Push

---
+++ src/components/IdCard.jsx
@@ -0,0 +1,17 @@
+function IdCard(props) {
+  return (
+    <div>
+      <p>First name : John</p>
+      <p>Last name : Doe</p>
+      <p>Gender : male </p>
+      <p>height : 1.78m</p>
+      <p>Birth : Tue Jul 14 1992</p>
+      <img
+        src="https://randomuser.me/api/portraits/men/44.jpg"
+        alt="photoUser"
+      />
+    </div>
+  );
+}
+
+export default IdCard;
